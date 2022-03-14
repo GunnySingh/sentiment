8cd166fdba0663896dc9abf8145164acf6f0f7f0
GunnySingh <gunpreetsingh72@gmail.com>
2022-03-15 00:54:10 +0530

    upload files

---
+++ app.py
@@ -0,0 +1,24 @@
+from email import message
+from flask import Flask,render_template,request
+import model
+
+app = Flask('__name__')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/recommend',methods=['POST'])
+def recommend_top5():
+    user_name = request.form['User Name']
+    
+    top20_products = model.recommend_product(user_name)
+    top5_products = model.Top_Products(top20_products)
+    
+    return render_template('index.html',column_names=top5_products.columns.values, row_data=list(top5_products.values.tolist()), zip=zip,text='Recommended products')
+    
+
+if __name__ == '__main__':
+    app.debug=False
+
+    app.run()
